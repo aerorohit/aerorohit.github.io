1d9eed590aba311e0ef60af984793fc9afb9bb16
Rohit Tembhare <rohittembhare1@gmail.com>
2025-02-13 00:41:32 +0530

    add email

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -14,7 +14,14 @@ export default function About() {
       </div>
       <h1 className="text-3xl font-bold mb-4 text-monokai-pink">About Me</h1>
       <p className="text-monokai-text max-w-2xl text-center mb-8">
-      Hello! I'm a software developer currently working at Uber. I hold both a Bachelor's and Master's degree in Aerospace Engineering. My interests include programming, computational fluid dynamics and robotics. I started this blog to document new things I learn along the way.
+        Hello! I'm a software developer currently working at Uber. I hold both a Bachelor's and Master's degree in Aerospace Engineering. My interests include programming, computational fluid dynamics and robotics. I started this blog to document new things I learn along the way.
+        You can reach out to me at <a
+          href="mailto:rohit@aerorohit.in"
+          className="text-monokai-yellow hover:text-monokai-text transition-colors"
+          aria-label="Email me at rohit@aerorohit.in"
+        >
+          rohit@aerorohit.in
+        </a>.
       </p>
       <div className="flex space-x-6">
         <a
